dfb34271ee8b856be60c366235bf140c1bae262c
Danilo Cecilia <danilo.cecilia@gmail.com>
2024-09-27 23:48:31 -0400

    Initial Setup

---
+++ lib/mongodb.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb'
+
+const uri = process.env.MONGODB_URI as string // Ensure the URI is a string
+
+let client: MongoClient
+let clientPromise: Promise<MongoClient>
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient>
+}
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  // In production mode, create a new client for each connection
+  client = new MongoClient(uri)
+  clientPromise = client.connect()
+}
+
+export default clientPromise
