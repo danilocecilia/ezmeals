da1e83c01491ab3e8a4b8840f0bf7e8aa30f1145
Danilo Cecilia <danilo.cecilia@gmail.com>
2024-10-06 22:26:24 -0400

    - oders sample

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 import { auth } from '@root/auth';
 import { redirect } from 'next/navigation';
 
-import DashboardForm from './Form';
+// import DashboardForm from './Form';
+import { DataTableDemo } from './Dashboard';
 
 const page = async () => {
   const session = await auth();
@@ -15,10 +16,9 @@ const page = async () => {
 
   return (
     <main className="max-w-7xl mx-auto my-12 space-y-5 container">
-      <h1 className="text-2xl font-semibold">
-        Welcome back, {user?.name || user?.email}
-      </h1>
-      <DashboardForm email={session?.user?.email as string} />
+      <h1 className="text-2xl font-semibold">Orders History</h1>
+      <DataTableDemo />
+      {/* <DashboardForm email={session?.user?.email as string} /> */}
     </main>
   );
 };
