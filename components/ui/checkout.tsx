2143fdc399ad7fa7867ea198ba72d0ca98b1d873
Danilo Cecilia <danilo.cecilia@gmail.com>
2024-11-07 00:02:44 -0500

    added checkout drawer component

--- components/ui/checkout.tsx
+++ components/ui/checkout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { DrawerMealCheckout } from '@components/DrawerMealCheckout';
 import { useCart } from '@root/context/CartContext';
 import { motion } from 'framer-motion';
 import { ShoppingCart } from 'lucide-react';
@@ -7,6 +8,7 @@ import React, { useState } from 'react';
 
 const Checkout = () => {
   const { state } = useCart();
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
 
   React.useEffect(() => {
     if (state.items.length > 0) {
@@ -21,35 +23,44 @@ const Checkout = () => {
   };
 
   const [focus, setFocus] = useState(false);
+
+  const handleCheckout = () => {
+    setIsDrawerOpen((prev) => !prev);
+  };
+
   return (
-    <div className="h-full w-full center">
-      <div className="relative z-0 ">
-        <motion.button
-          onMouseEnter={() => setFocus(true)}
-          onMouseLeave={() => setFocus(false)}
-          className="h-12 px-10 overflow-hidden z-10 flex items-center gap-2 rounded-xl"
-        >
-          <span className="relative flex w-10 h-10 hover:bg-violet-100 justify-center items-center rounded-full">
-            <ShoppingCart className="h-5 w-5" />
-            {state.items.length > 0 && (
-              <motion.span
-                animate={{
-                  y: focus ? 0 : '100%',
-                  opacity: focus ? 1 : 0
-                }}
-                transition={{
-                  duration: 0.2
-                }}
-                className="text-xs h-4 w-4 rounded-full bg-primary text-primary-foreground -top-1 -right-1.5 absolute"
-              >
-                {state.items.length > 0 ? getTotalQuantity() : ''}
-              </motion.span>
-            )}
-          </span>
-          <span className="text-md font-semibold">Cart</span>
-        </motion.button>
+    <>
+      <DrawerMealCheckout isOpen={isDrawerOpen} onClose={setIsDrawerOpen} />
+      <div className="h-full w-full center">
+        <div className="relative z-0 ">
+          <motion.button
+            onClick={() => handleCheckout()} // Add onClick handler
+            onMouseEnter={() => setFocus(true)}
+            onMouseLeave={() => setFocus(false)}
+            className="h-12 px-10 overflow-hidden z-10 flex items-center gap-2 rounded-xl"
+          >
+            <span className="relative flex w-10 h-10 hover:bg-violet-100 justify-center items-center rounded-full">
+              <ShoppingCart className="h-5 w-5" />
+              {state.items.length > 0 && (
+                <motion.span
+                  animate={{
+                    y: focus ? 0 : '100%',
+                    opacity: focus ? 1 : 0
+                  }}
+                  transition={{
+                    duration: 0.2
+                  }}
+                  className="text-xs h-4 w-4 rounded-full bg-primary text-primary-foreground -top-1 -right-1.5 absolute"
+                >
+                  {state.items.length > 0 ? getTotalQuantity() : ''}
+                </motion.span>
+              )}
+            </span>
+            <span className="text-md font-semibold">Cart</span>
+          </motion.button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
