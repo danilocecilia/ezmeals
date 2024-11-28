4d20e1ac11d7a7dcd3ee0e266a717866b7517724
Danilo Cecilia <danilo.cecilia@gmail.com>
2024-11-28 17:03:04 -0500

    - update user profile page

--- app/profile/Profile.tsx
+++ app/profile/Profile.tsx
@@ -1,42 +1,15 @@
 'use client';
 
 import { UserProps } from '@types';
-import { useState } from 'react';
 
 import ProfileForm from './Form';
-import NavigationButton from './NavigationButton';
-import Orders from './Orders';
 
-type IconType = 'profile' | 'orders';
-
-const UserProfile = ({ user }: { user: UserProps | undefined }) => {
-  const [selectedSection, setSelectedSection] = useState('profile');
-
-  const sections: { type: IconType; title: string }[] = [
-    { type: 'profile', title: 'User Profile' },
-    { type: 'orders', title: 'Orders' }
-  ];
-
-  const handleNavigationClick = (type: IconType) => {
-    setSelectedSection(type);
-  };
-
+const UserProfile = ({ user }: { user: UserProps }) => {
   return (
-    <div className="px-4">
-      <div className="grid grid-cols-1 lg:grid-cols-2 lg:divide-x h-[800px]">
-        <div className="flex flex-row lg:flex-col justify-center items-end space-y-8">
-          {sections.map((section) => (
-            <NavigationButton
-              type={section.type}
-              key={section.type}
-              title={section.title}
-              isSelected={selectedSection === section.type}
-              onClick={handleNavigationClick}
-            />
-          ))}
-        </div>
-        {selectedSection == 'profile' && user && <ProfileForm user={user} />}
-        {selectedSection == 'orders' && <Orders />}
+    <div className="flex flex-col p-4">
+      <h1 className="text-2xl font-bold text-center">Edit Profile</h1>
+      <div className="grid grid-cols-1 lg:divide-x h-[800px]">
+        <ProfileForm user={user} />
       </div>
     </div>
   );
